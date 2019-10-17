23584e8cbc06feb3c75b104502679163a844d08d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-10-17 19:02:59 -0400

    Added comments. Ignore the warnings for now, I will go back to fix them later.

--- src/app/services/card-service.service.ts
+++ src/app/services/card-service.service.ts
@@ -6,6 +6,8 @@ import { CardModel } from '../models/card.model';
 })
 export class CardServiceService {
  private cards: CardModel[] = [
+
+   // The file names are meaningless.
     new CardModel(1, 'Image Path', 'AA1.mp4', 'C:\\Users\\dakil\\Desktop\\Extras'),
     new CardModel(2, 'Image path', 'AA2.mp4', 'C:\\Users\\dakil\\Desktop\\Extras'),
     new CardModel(3, 'Image path', 'AA4.mp4', 'C:\\Users\\dakil\\Desktop\\Extras'),
