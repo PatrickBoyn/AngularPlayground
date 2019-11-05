0f06657a69e5c62dcf525efc3adb391c3e28f062
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-11-05 17:34:57 -0500

    Called the new card service in the place of the old logic.

--- src/app/cards/create-card/create-card.component.ts
+++ src/app/cards/create-card/create-card.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {HttpClient} from '@angular/common/http';
+import {CrudService} from '../../services/crud.service';
 
 @Component({
   selector: 'app-create-card',
@@ -15,17 +16,18 @@ export class CreateCardComponent implements OnInit {
     filePath: ''
   };
   isSubmitted = false;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private crudService: CrudService) { }
 
   ngOnInit() {
 
   }
 
   onCreateCard(postData: {fileTitle: string, filePath: string}) {
-    this.http.post('https://fileserverproject-1e496.firebaseio.com/files.json', postData)
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
+    // this.http.post('https://fileserverproject-1e496.firebaseio.com/files.json', postData)
+    //   .subscribe(responseData => {
+    //     console.log(responseData);
+    //   });
+    console.log(this.crudService.createCard(postData));
   }
 
 }
