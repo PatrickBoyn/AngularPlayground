a79f2db882af04c9a38ae2d8d92a41e411d8cdd1
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-11-05 17:34:34 -0500

    Moved the logic to the service for creating a card.

--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  createCard(cardData: { fileTitle: string, filePath: string}) {
+    this.http.post('https://fileserverproject-1e496.firebaseio.com/files.json', cardData)
+      .subscribe(response => {
+        console.log(`Response ${response}`);
+      });
+    console.log(`Card Data: ${cardData}`);
+  }
 }
