834b6b8fc502395b8db495510578635e10871a52
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-11-06 13:55:03 -0500

    Removed commented out code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,5 @@ import {CardModel} from './models/card.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // @Output() cards: CardModel[] = [
-  //   new CardModel('Image Path', 'AA1.mp4', 'C:\\Users\\dakil\\Desktop\\Extras'),
-  //   new CardModel('Image path', 'AA2.mp4', 'C:\\Users\\dakil\\Desktop\\Extras'),
-  //   new CardModel('Image path', 'AA4.mp4', 'C:\\Users\\dakil\\Desktop\\Extras'),
-  //   new CardModel('Image path', 'AF.mp4', 'C:\\Users\\dakil\\Desktop\\Extras')
-  // ];
   title = 'angular-playground';
 }
