2ae1fae0ce8dbe49ca5b63e63b426a4fca8579fc
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-11-10 16:42:08 -0500

    Added the same logic that was previously in the card, to the parent (cards).

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,20 +1,29 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {CardModel} from '../models/card.model';
 import {ActivatedRoute, Params} from '@angular/router';
 import {CardService} from '../services/card.service';
 import {CrudService} from '../services/crud.service';
+import {Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css']
 })
-export class CardsComponent implements OnInit {
-  @Input()cards: any;
+export class CardsComponent implements OnInit, OnDestroy {
+  @Output() card: Subscription;
+  @Output() cardList: CardModel[];
 
-  constructor(private route: ActivatedRoute, private crudService: CrudService) {}
+  constructor(private crudService: CrudService) {}
 
   ngOnInit() {
-    this.cards = this.crudService.loadedCards;
+    this.cardList = this.crudService.loadedCards;
+    if (this.cardList.length === 0) {
+      this.card = this.crudService.getCards().subscribe();
+    }
+  }
+
+  ngOnDestroy() {
+    this.card.unsubscribe();
   }
 }
