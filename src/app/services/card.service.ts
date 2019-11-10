561d21b04400310087f6875e75c3b3c0406e8912
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-11-10 16:46:57 -0500

    Commented out the code to see if the service can be deleted.

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -5,22 +5,22 @@ import { CardModel } from '../models/card.model';
   providedIn: 'root'
 })
 export class CardService {
- private cards: CardModel[] = [
-
-   // The file names are meaningless.
-    new CardModel(1,  'AA1.mp4', 'C:\\Users\\dakil\\Desktop\\Extras'),
-    new CardModel(2,  'AA2.mp4', 'C:\\Users\\dakil\\Desktop\\Extras'),
-    new CardModel(3,  'AA4.mp4', 'C:\\Users\\dakil\\Desktop\\Extras'),
-    new CardModel(4,  'AF.mp4', 'C:\\Users\\dakil\\Desktop\\Extras')
-  ];
-  constructor() { }
-
-  getCards(): CardModel[] {
-    return this.cards.slice();
-  }
-
-  getCard(id: number): CardModel  {
-    console.log(`ID is: ${id}`);
-    return this.cards[id];
-  }
+ // private cards: CardModel[] = [
+ //
+ //   // The file names are meaningless.
+ //   //  new CardModel(1,  'AA1.mp4', 'C:\\Users\\dakil\\Desktop\\Extras'),
+ //   //  new CardModel(2,  'AA2.mp4', 'C:\\Users\\dakil\\Desktop\\Extras'),
+ //   //  new CardModel(3,  'AA4.mp4', 'C:\\Users\\dakil\\Desktop\\Extras'),
+ //   //  new CardModel(4,  'AF.mp4', 'C:\\Users\\dakil\\Desktop\\Extras')
+ //  ];
+ //  constructor() { }
+ //
+ //  getCards(): CardModel[] {
+ //    return this.cards.slice();
+ //  }
+ //
+ //  getCard(id: number): CardModel  {
+ //    console.log(`ID is: ${id}`);
+ //    return this.cards[id];
+ //  }
 }
