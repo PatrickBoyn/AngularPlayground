b134d4f317813faa6848bdd2997650c319715c89
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-11-10 16:39:55 -0500

    Added an input card to pass the loop data down. Removed all unwanted code.

--- src/app/cards/card/card.component.ts
+++ src/app/cards/card/card.component.ts
@@ -1,20 +1,22 @@
-import {Component, OnInit, Input, Output} from '@angular/core';
+import {Component, OnInit, Input, Output, OnDestroy} from '@angular/core';
 import { CardModel } from '../../models/card.model';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {CrudService} from '../../services/crud.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
- @Input() card: any;
- @Input() id: number;
-
-  constructor(private route: ActivatedRoute, private crudService: CrudService) { }
+export class CardComponent implements OnInit, OnDestroy {
+  @Input() card;
+  constructor(private route: ActivatedRoute, private crudService: CrudService) {
+  }
 
   ngOnInit() {
-    this.card = this.crudService.loadedCards;
+  }
+
+  ngOnDestroy() {
   }
 }
