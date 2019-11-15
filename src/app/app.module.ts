d6ccf8d92b507266326407055f787262f039702d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-11-14 22:08:24 -0500

    Moved everything except the navbar into it's own module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { CardComponent } from './cards/card/card.component';
-import { CreateCardComponent } from './cards/create-card/create-card.component';
 import { SearchFilesComponent } from './search-files/search-files.component';
-import { HomeComponent } from './home/home.component';
-import { CardsComponent } from './cards/cards.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {CardsModule} from './cards/cards.module';
+import {HomeModule} from './home/home/home.module';
+import {SearchFilesModule} from './search-files/search-files/search-files.module';
+import {PageNotFoundModule} from './page-not-found/page-not-found/page-not-found.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SearchFilesComponent,
-    HomeComponent,
-    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    CardsModule
+    CardsModule,
+    HomeModule,
+    SearchFilesModule,
+    PageNotFoundModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search-files/search-files/search-files.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {SearchFilesComponent} from '../search-files.component';
+
+
+
+@NgModule({
+  declarations: [SearchFilesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SearchFilesModule { }
